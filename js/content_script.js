c772118577943557ae080654e43db4759a75f3a4
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2019-02-26 22:04:28 +0200

    some change

--- js/content_script.js
+++ js/content_script.js
@@ -1,41 +1,75 @@
-function getTextNode(content) {
-    var treeWalker = document.createTreeWalker(content, NodeFilter.SHOW_TEXT, null, false);
-    var containerNodSelects = [];
-    while (treeWalker.nextNode()) {
-        containerNodSelects.push(treeWalker.currentNode);
+function getTextNodeClone() {
+    var content1 = window.getSelection().getRangeAt(0).cloneContents();
+    var treeWalker1 = document.createTreeWalker(content1, NodeFilter.SHOW_TEXT, null, false);
+    var text1 = [];
+    while (treeWalker1.nextNode()) {
+        if (treeWalker1.currentNode.data.includes("\n") ||
+            treeWalker1.currentNode.length === 0 ||
+            treeWalker1.currentNode.data === " ") {
+            continue
+        }
+
+        text1.push(treeWalker1.currentNode);
+
     }
-    return containerNodSelects;
+
+    return text1;
+}
+
+function getTextNodeCommon(len) {
+    var content = window.getSelection().getRangeAt(0).commonAncestorContainer;
+    var treeWalker = document.createTreeWalker(content, NodeFilter.SHOW_TEXT, null, false);
+    var text = [];
+    while (treeWalker.nextNode()) {
+        if (treeWalker.currentNode.data.includes("\n") ||
+            treeWalker.currentNode.length === 0 ||
+            treeWalker.currentNode.data === " ") {
+            continue;
+        }
+        text.push(treeWalker.currentNode);
+    }
+    if (text.length === 0) {
+        text.push(treeWalker.currentNode);
+    }
+
+    if (len + 1 === text.length) {
+        text.splice(0, 1);
+    } else {
+        text.splice(len);
+
+    }
+    return text;
+
 }
 
 function highlightSelection() {
 
     reloadSettings(function () {
-        var content = window.getSelection().getRangeAt(0).startContainer;
+        var selectsClone = getTextNodeClone();
+        var selectsCommon = getTextNodeCommon(selectsClone.length);
+        var elementCommon;
+        var elementClone;
+        var indexForSplit;
+        var startIndex, innerHTML;
 
-        var containerNodSelects = getTextNode(content);
-        var selectRange;
-        for (var startNode = 0; startNode < containerNodSelects.length; startNode++) {
-            // if (containerNodeSafe[startNode].endOffset === 0){
-            var node = addNewNodeToSelectRange(containerNodSelects[startNode]);
-            selectRange = containerNodSelects[startNode];
-            selectRange.parentNode.replaceChild(node, selectRange);
-            // selectRange.insertNode(node);
-            // }
+        for (var i = 0; i < selectsCommon.length; i++) {
+            elementCommon = selectsCommon[i];
+            elementClone = selectsClone[i];
+
+            indexForSplit = elementCommon.data.indexOf(elementClone.data);
+            startIndex = elementCommon.parentElement.innerHTML.indexOf(elementCommon.data);
+            innerHTML = elementCommon.parentElement.innerHTML.substring(startIndex, elementClone.length);
+            elementCommon.parentElement.innerHTML =
+                elementCommon.parentElement.innerHTML.substring(0, indexForSplit)
+                + "<span style=\"background-color: " + currentColor + "\">"
+                + elementCommon.parentElement.innerHTML.substring(indexForSplit, indexForSplit === 0 ? elementClone.length : elementClone.length + 1)
+                + "</span>"
+                + elementCommon.parentElement.innerHTML.substring(elementClone.length + indexForSplit, elementClone.length);
+
         }
     });
 }
 
-
-function addNewNodeToSelectRange(range) {
-
-    var newNodeWithBackgroundColor = document.createElement("span");
-    newNodeWithBackgroundColor.style.backgroundColor = currentColor;
-    newNodeWithBackgroundColor.style.color = "black";
-
-    newNodeWithBackgroundColor.appendChild(range.cloneNode(true));
-
-    return newNodeWithBackgroundColor;
-}
 
 function callHighlightSelection(event) {
     if (event.key === currentShortcut) {
