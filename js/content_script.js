1a99cb94bd56fe8f70e36c4d20d86e1df7f0bc65
Abuhay Abune <abuhay.abune@cs.huji.ac.il>
2019-02-27 12:20:05 +0200

    solve a bug the break a html tree

--- js/content_script.js
+++ js/content_script.js
@@ -1,75 +1,69 @@
-function getTextNodeClone() {
-    var content1 = window.getSelection().getRangeAt(0).cloneContents();
-    var treeWalker1 = document.createTreeWalker(content1, NodeFilter.SHOW_TEXT, null, false);
-    var text1 = [];
-    while (treeWalker1.nextNode()) {
-        if (treeWalker1.currentNode.data.includes("\n") ||
-            treeWalker1.currentNode.length === 0 ||
-            treeWalker1.currentNode.data === " ") {
-            continue
+function getTextFromCommonAncestorNode() {
+    var range = window.getSelection().getRangeAt(0);
+    var treeWalker = document.createTreeWalker(
+        range.commonAncestorContainer,
+        NodeFilter.SHOW_TEXT,
+        {
+            acceptNode: function (node) {
+                if (range.isPointInRange(node, 0))
+                    return NodeFilter.FILTER_ACCEPT;
+            }
+        },
+        false
+    );
+
+    var node;
+    var nodes = [];
+    while (node = treeWalker.nextNode()) {
+        if (node.data.replace(/[\s]+/, "").length) {
+            nodes.push(node);
         }
-
-        text1.push(treeWalker1.currentNode);
-
     }
 
-    return text1;
-}
-
-function getTextNodeCommon(len) {
-    var content = window.getSelection().getRangeAt(0).commonAncestorContainer;
-    var treeWalker = document.createTreeWalker(content, NodeFilter.SHOW_TEXT, null, false);
-    var text = [];
-    while (treeWalker.nextNode()) {
-        if (treeWalker.currentNode.data.includes("\n") ||
-            treeWalker.currentNode.length === 0 ||
-            treeWalker.currentNode.data === " ") {
-            continue;
-        }
-        text.push(treeWalker.currentNode);
-    }
-    if (text.length === 0) {
-        text.push(treeWalker.currentNode);
+    // if it si only one words
+    if (nodes.length === 0) {
+        var startNode = range.startContainer;
+        var startOffset = range.startOffset;
+        var endOffset = range.endOffset;
+        node = startNode.splitText(startOffset);
+        node.splitText(endOffset - startOffset);
+        nodes.push(node);
+        return nodes;
     }
 
-    if (len + 1 === text.length) {
-        text.splice(0, 1);
-    } else {
-        text.splice(len);
-
+    // for last word
+    if (!nodes.includes(range.endContainer)) {
+        nodes.push(range.endContainer);
     }
-    return text;
+    //for first word
+    if (!nodes.includes(range.startContainer)) {
+        nodes.push(range.startContainer);
+    }
+
+    return nodes;
 
 }
 
 function highlightSelection() {
 
     reloadSettings(function () {
-        var selectsClone = getTextNodeClone();
-        var selectsCommon = getTextNodeCommon(selectsClone.length);
-        var elementCommon;
-        var elementClone;
-        var indexForSplit;
-        var startIndex, innerHTML;
+        var selectsTextNode = getTextFromCommonAncestorNode();
 
-        for (var i = 0; i < selectsCommon.length; i++) {
-            elementCommon = selectsCommon[i];
-            elementClone = selectsClone[i];
-
-            indexForSplit = elementCommon.data.indexOf(elementClone.data);
-            startIndex = elementCommon.parentElement.innerHTML.indexOf(elementCommon.data);
-            innerHTML = elementCommon.parentElement.innerHTML.substring(startIndex, elementClone.length);
-            elementCommon.parentElement.innerHTML =
-                elementCommon.parentElement.innerHTML.substring(0, indexForSplit)
-                + "<span style=\"background-color: " + currentColor + "\">"
-                + elementCommon.parentElement.innerHTML.substring(indexForSplit, indexForSplit === 0 ? elementClone.length : elementClone.length + 1)
-                + "</span>"
-                + elementCommon.parentElement.innerHTML.substring(elementClone.length + indexForSplit, elementClone.length);
-
-        }
+        selectsTextNode.forEach(function (value) {
+            doSpan(value);
+        });
     });
 }
 
+function doSpan(range) {
+
+    var span = document.createElement("span");
+    span.style.backgroundColor = currentColor;
+    span.style.color = "black";
+    var clone = range.cloneNode(true);
+    span.appendChild(clone);
+    range.parentNode.replaceChild(span, range);
+}
 
 function callHighlightSelection(event) {
     if (event.key === currentShortcut) {
